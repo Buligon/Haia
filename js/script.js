535368bb7cfa4d391c9f8c441299b4bba26e20ba
Lucas Buligon Antunes <lukasbuligonantunes@gmail.com>
2021-07-17 15:25:20 -0300

    Adicionado menu lateral

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-$('.abreMenuLateral').click(function(){
-        $('.menuLateral').toggleClass('.mostra');
-});
+function teste() {
+        console.log('Click');
+        var foo = document.querySelector('.menuLateral, .menuLateralmostra');
+
+        if (foo.classList.contains("menuLateral")) {
+                foo.classList.replace("menuLateral","menuLateralmostra");
+        } else {
+                foo.classList.replace("menuLateralmostra","menuLateral");
+        }
+        
+}
