9ff2c995372e8af6b41528c843a2b5d740dae39f
Lucas Buligon Antunes <lukasbuligonantunes@gmail.com>
2021-08-03 10:17:24 -0300

    Adicição do JS do bootstrap para correção de erros

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function abreMenu() {
+/* function abreMenu() {
         var foo = document.querySelector('.menuLateral, .menuLateralmostra');
 
         if (foo.classList.contains("menuLateral")) {
@@ -6,5 +6,23 @@ function abreMenu() {
         } else {
                 foo.classList.replace("menuLateralmostra","menuLateral");
         }
-        
+
+} */
+
+function abreMenu(event) {
+        var htmlContent = '';
+
+
+        switch (event.target.value) {
+                case 'A': {
+                        
+                        htmlContent = "<h1>tyeste</h1>";
+                        break;
+                }
+                case 'B': {
+                        htmlContent = "content for B";
+                        break;
+                }
+        }
+        document.getElementById('menuLatS').innerHTML = htmlContent;
 }
