59c1562f4ddd3d20d32af8ca7adef2a6b6412466
Lucas Buligon Antunes <lukasbuligonantunes@gmail.com>
2021-08-05 10:36:17 -0300

    Nova versão do menu lateral

--- js/script.js
+++ js/script.js
@@ -1,28 +1,17 @@
-/* function abreMenu() {
-        var foo = document.querySelector('.menuLateral, .menuLateralmostra');
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
 
-        if (foo.classList.contains("menuLateral")) {
-                foo.classList.replace("menuLateral","menuLateralmostra");
-        } else {
-                foo.classList.replace("menuLateralmostra","menuLateral");
-        }
+closeBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("open");
+  menuBtnChange();//calling the function(optional)
+});
 
-} */
-
-function abreMenu(event) {
-        var htmlContent = '';
-
-
-        switch (event.target.value) {
-                case 'A': {
-                        
-                        htmlContent = "<h1>tyeste</h1>";
-                        break;
-                }
-                case 'B': {
-                        htmlContent = "content for B";
-                        break;
-                }
-        }
-        document.getElementById('menuLatS').innerHTML = htmlContent;
+// following are the code to change sidebar button(optional)
+function menuBtnChange() {
+ if(sidebar.classList.contains("open")){
+   closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+ }else {
+   closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+ }
 }
