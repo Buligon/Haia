c04ba541a54262e9835b5fdcfdde2ce3fde6f962
Lucas Buligon Antunes <lukasbuligonantunes@gmail.com>
2021-12-19 01:57:00 -0300

    Alteração de aparência e tela agora é dinâmica

--- server/models/Projeto.js
+++ server/models/Projeto.js
@@ -1,8 +1,15 @@
 const db = require('../config/database');
 
 const Projeto = db.sequelize.define('Projetos', {
+  idProjetos: {
+    type: db.Sequelize.INTEGER,
+    primaryKey: true
+  },
   nomeProjeto: {
     type: db.Sequelize.STRING
+  },
+  codigoCriador: {
+    type: db.Sequelize.INTEGER,
   },
   descricao: {
     type: db.Sequelize.TEXT
--- server/routes/main.js
+++ server/routes/main.js
@@ -2,53 +2,62 @@ const express = require("express");
 const router = express.Router();
 
 const Usuario = require('../models/Usuario.js');
+const Projeto = require('../models/Projeto.js');
+
 const { sequelize } = require("../config/database.js");
 router.get('/', (req, res) => {
-  res.render('login');
+  res.render('login', {
+    style: 'styles.css'
+  });
 });
 
 router.get('/cadUsuario', (req, res) => {
-  res.render('cadastros/cadUsuario');
+  res.render('cadastros/cadUsuario', {
+    style: 'styles.css'
+  });
 });
 
 router.post('/addUsuario', (req, res) => {
   var erros = [];
 
   // Valida se nome foi preenchido corretamente
-  if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
-    erros.push({texto: "Nome inválido!"});
+  if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
+    erros.push({ texto: "Nome inválido!" });
   }
 
   // Valida se email foi preenchido corretamente
-  if(!req.body.email || typeof req.body.email == undefined || req.body.email == null) {
-    erros.push({texto: "Email inválido!"});
+  if (!req.body.email || typeof req.body.email == undefined || req.body.email == null) {
+    erros.push({ texto: "Email inválido!" });
   }
 
   // Valida se as senhas foram preenchidas corretamente
-  if(!req.body.pwd || typeof req.body.pwd == undefined || req.body.pwd == null || !req.body.pwd2 || typeof req.body.pwd2 == undefined || req.body.pwd2 == null) {
-    erros.push({texto: "Senha inválida!"});
+  if (!req.body.pwd || typeof req.body.pwd == undefined || req.body.pwd == null || !req.body.pwd2 || typeof req.body.pwd2 == undefined || req.body.pwd2 == null) {
+    erros.push({ texto: "Senha inválida!" });
   }
 
-  if(req.body.pwd.length < 5) {
-    erros.push({texto: "Senha muito curta!"});
+  if (req.body.pwd.length < 5) {
+    erros.push({ texto: "Senha muito curta!" });
   }
 
-  if(req.body.pwd.length < 5) {
-    erros.push({texto: "Senha muito curta!"});
+  if (req.body.pwd.length < 5) {
+    erros.push({ texto: "Senha muito curta!" });
   }
 
-  if(req.body.pwd != req.body.pwd2) {
-    erros.push({texto: "Senhas não coincidem!"});
+  if (req.body.pwd != req.body.pwd2) {
+    erros.push({ texto: "Senhas não coincidem!" });
   }
 
   if (erros.length > 0) {
-    res.render("cadastros/cadUsuario", {erros: erros});
+    res.render("cadastros/cadUsuario", {
+      style: 'styles.css',
+      erros: erros
+    });
   } else {
 
-    Usuario.findAll({where:{email: req.body.email}}).then((usuario) => {
-      if(!(JSON.stringify(usuario) == `[]`)){
+    Usuario.findAll({ where: { email: req.body.email } }).then((usuario) => {
+      if (!(JSON.stringify(usuario) == `[]`)) {
         req.flash("error_msg", "já existe uma conta com esse email!")
-        res.redirect("/cadUsuario")
+        res.redirect("cadUsuario")
       } else {
         Usuario.create({
           nomeUsuario: req.body.nome,
@@ -60,21 +69,32 @@ router.post('/addUsuario', (req, res) => {
           res.redirect('listaProjetos')
         }).catch(function (erro) {
           req.flash("error_msg", "Houve um erro ao criar o usuário, tente novamente!");
-          res.redirect('cadUsuario')
+          res.redirect('cadUsuario', {
+            style: 'styles.css'
+          })
         });
       }
     }).catch((err) => {
       req.flash("error_msg", "Houve um erro interno");
       res.redirect("cadUsuario")
-    }) 
+    })
 
 
-  } 
+  }
 
 });
 
-router.get('/listaProjetos', (req, res) => {
-  res.render('listaProjetos');
+router.get('/listaProjetos', async (req, res) => {
+  await Projeto.findAll().then((projetos) => {
+    res.render('listaProjetos', {
+      layout: 'listaProjetosLayout.hbs',
+      style: 'styles.css',
+      projetos: projetos.map(projetos => projetos.toJSON()) 
+    });
+  }).catch((err) => {
+    req.flash("error_msg", "Houve um erro ao listar os projetos!" + JSON.stringify(err));
+    res.redirect("/")
+  })
 });
 
 module.exports = router;
